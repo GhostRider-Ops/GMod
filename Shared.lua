6ce31c621521c86fba7531ffedb78e7e01af3d3d
John Reardon <john.t.s.reardon@icloud.com>
2025-05-22 15:56:36 -0400

    Create Shared.lua

---
+++ Shared.lua
@@ -0,0 +1,11 @@
+GM.Name	= "____________"
+GM.Author	= "____________"
+GM.Email	= "____________"
+GM.Website  = "____________"
+
+DeriveGamemode( "____________" )
+
+team.SetUp( 1, "Admin", Color( 255, 255, 255, 255 ) )
+team.SetUp( 2, "Guest", Color( 125, 125, 125, 255 ) )
+team.SetUp( 3, "____________", Color( 148, 0, 211, 255 ) )
+team.SetUp( 4, "Joining", Color( 0, 0 , 0, 255 ) )
